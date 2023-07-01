f65c04180cdedd631e51f199fd57db3c1874e070
lex0ver <luisenriquelozanomejia@gmail.com>
2023-07-01 14:32:30 -0500

    add cart

--- main.js
+++ main.js
@@ -3,15 +3,26 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const cartMenuIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+cartMenuIcon.addEventListener('click', toggleCart);
 
 function toggleDesktopMenu(){
+    
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCart(){
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
+
 }
